043835c7e6b24b0b2af2ddf71e3026c59b592051
clairehu <471339423@qq.com>
2016-09-16 22:42:02 +0800

    同步JSPatch

--- HKJSPatchDemo/HKJSPatchDemo/AppDelegate.m
+++ HKJSPatchDemo/HKJSPatchDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "JPEngine.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [JPEngine startEngine];
+
     return YES;
 }
 
